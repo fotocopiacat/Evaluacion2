23a657b151f9c4df9225bed71a8132f8e636d1bf
Lore Nita <32942173+lanavia@users.noreply.github.com>
2018-12-14 19:06:37 -0300

    clase producto

---
+++ app/src/main/java/com/example/android/evaluacion2/Product.kt
@@ -0,0 +1,8 @@
+package com.example.android.evaluacion2
+
+class Product(var ID:Int,
+              var Name:String,
+              var stock: Int,
+              var price: Double,
+              var category: String) {
+}
