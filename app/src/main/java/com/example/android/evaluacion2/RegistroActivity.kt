64e4b4e6a7f42fdeb3bd30da37e6d3169826fc9d
fotocopia <catalina.pimentel@uniacc.edu>
2018-12-14 19:17:37 -0300

    creacion layout registro

---
+++ app/src/main/java/com/example/android/evaluacion2/RegistroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.evaluacion2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
